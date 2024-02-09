23bbf06f12201c59795e8b00450ce727b24f71da
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-02-09 16:16:05 +0200

    Express added

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Welcome to express JS');
+import  express  from "express";
+
+// create an express app
+const app = express()
+const port = +process.env.port || 4000
+// Router 
+app.get('/', (req,res)=>{
+// res.status(200).json({
+//     msg: 'You\'re home'
+// }) 
+res.json({
+    status: res.statusCode,
+    msg: 'You\'re home'
+})
+})
+app.get('/about', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        msg: 'About page'
+    })
+})
+app.all('*', (req, res)=>{
+    res.json({
+        status: 404,
+        msg: '404 page'
+    })
+})
+app.listen(port)
