d49aa8ecc8c60a424a0ca5e807795aa3bf761e2f
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-02-12 14:36:46 +0200

    margin not working

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 import  express  from "express";
-
+import path from "path";
 // create an express app
 const app = express()
+const router = express.Router()
+// using router as middleware
+app.use(router,
+    express.static('./static'))
 const port = +process.env.port || 4000
 // Router 
-app.get('/', (req,res)=>{
-// res.status(200).json({
+router.get('^/$|/express',display, (req,res)=>{
+res.status(200).sendFile(path.resolve('./static/html/index.html'))
 //     msg: 'You\'re home'
 // }) 
-res.json({
-    status: res.statusCode,
-    msg: 'You\'re home'
+// res.json({
+//     status: res.statusCode,
+//     msg: 'You\'re home'
+// })
 })
-})
-app.get('/about', (req, res)=>{
+router.get('/about', (req, res)=>{
     res.json({
         status: res.statusCode,
         msg: 'About page'
     })
 })
-app.all('*', (req, res)=>{
+router.all('*', (req, res)=>{
     res.json({
         status: 404,
         msg: '404 page'
     })
 })
+// Middleware
+function display(req, res, next) {
+    console.log("Hello there");
+    next()
+    
+} 
 app.listen(port)
